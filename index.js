f85963e6ab27d0f03c69972bea797312eaf33210
amiedawn <amie.d.carroll@gmail.com>
2021-11-24 16:37:29 -0700

    rename files and add gridlines to output

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-let inputForm = document.querySelector('#input-form');
-let parentFirstName = document.querySelector("#parent-first-name");
-let parentLastName = document.querySelector("#parent-last-name");
-let email = document.querySelector("#email");
-let childName = document.querySelector("#child-name");
-let grade = document.querySelector("#grade");
-let tableHeader = document.getElementById("#table-header");
-let table = document.getElementById("#myTable");
-let button = document.querySelector("#submit");
+function addRecord() {
+ 
+  let parentFirstName = document.getElementById("parent-first-name").value;
+  let parentLastName = document.getElementById("parent-last-name").value;
+  let email = document.getElementById("email").value;
+  let childName = document.getElementById("child-name").value;
+  let grade = document.getElementById("grade").value;
+  let tableHeader = document.getElementById("table-header");
 
-// define class Parent with class declaration
-class Parent {
+  class Parent {
   constructor(parentFirstName, parentLastName, email, childName, grade) {
     this.parentFirstName = parentFirstName;
     this.parentLastName = parentLastName;
@@ -17,149 +15,44 @@ class Parent {
     this.childName = childName;
     this.grade = grade;
   }
-  getInfo() {
-    return (
-      this.parentFirstName,
-      this.parentLastName,
-      this.email,
-      this.childName,
-      this.grade
-    );
-  }
-}
+    getInfo() {
+      return (
+        this.parentFirstName,
+        this.parentLastName,
+        this.email,
+        this.childName,
+        this.grade
+      );
+    }
+  };
 
-// create extra instances of the object
-let parent1 = new Parent("Joe", "Brown", "joe@email.com", "Suzy", 2);
-parent1.getInfo();
+  // extra instances of the object
+  const parentObj = new Parent(
+    parentFirstName,
+    parentLastName,
+    email,
+    childName,
+    grade
+  );
+  parentObj.getInfo();
 
-let parent2 = new Parent("Sal", "Meyer", "sal@email.com", "Ava", 4);
-parent2.getInfo();
-
-let parent3 = new Parent("Sheri", "Jones", "sheri@email.com", "Tommy", 3);
-parent3.getInfo();
-
-// array of all objects
-let allParents = [parent1, parent2, parent3];
-
-// array of all object names
-let allParentsName = [
-  parent1.parentFirstName,
-  parent2.parentFirstName,
-  parent3.parentFirstName,
-];
-
-// read input form
-function readInputForm(e) {
-  e.preventDefault();
-
-  const containerInput = document.querySelectorAll("#container-input");
-  const values = Array.from(containerInput).map((el) => el.value);
-  const res = allParents.filter((a) => values.includes(a.parentFirstName));
-  display(res);
+  let table = tableHeader.insertRow(tableHeader.length);
+  let cell1 = table.insertCell(0);
+  cell1.innerHTML = parentObj.parentFirstName;
+  let cell2 = table.insertCell(1);
+  cell2.innerHTML = parentObj.parentLastName;
+  let cell3 = table.insertCell(2);
+  cell3.innerHTML = parentObj.email;
+  let cell4 = table.insertCell(3);
+  cell4.innerHTML = parentObj.childName;
+  let cell5 = table.insertCell(4);
+  cell5.innerHTML = parentObj.grade;
+  let cell6 = table.insertCell(5);
+  cell6.innerHTML = "<input type='button' value='Edit' onclick='editRecord(this)' />";
+  let cell7 = table.insertCell(6);
+  cell7.innerHTML = "<input type='button' value='Delete' onclick='deleteRecord(this)' />";
 };
 
+function deleteRecord(record) {
 
-// onsubmit event
-// button.addEventListener("click", (e) => {
-//   e.preventDefault();
-
-//   const containerInput = document.querySelectorAll("#container-input");
-//   const values = Array.from(containerInput).map((el) => el.value);
-//   const res = allParents.filter((a) => values.includes(a.parentFirstName));
-//   display(res);
-// });
-
-// create new record
-function createRecord() {
-  alert("createRecord works");
-  let tbodyEl = document.querySelectorAll("#users");
-  let newRow = tbodyEl[0].insertRow();
-  return newRow;
 };
-
-// create new cell
-function createData(el, newTr) {
-  alert("create data works");
-  let newEl = document.createElement("td");
-  let elementId = document.querySelectorAll("td").length;
-  newEl.setAttribute("id", elementId);
-  newEl.innerHTML = el;
-  newTr.appendChild(newEl);
-};
-
-// print new record
-function display(element) {
-  alert("display data");
-  element.forEach((e) => {
-    const newTr = createRecord();
-    createData(e.parentFirstName, newTr);
-    createData(e.parentLastNme, newTr);
-    createData(e.email, newTr);
-    createData(e.childName, newTr);
-    createData(e.grade, newTr);
-  });
-};
-
-// create columns
-
-// let cell1 = row.insertCell(0);
-// let cell2 = row.insertCell(1);
-// let cell3 = row.insertCell(2);
-// let cell4 = row.insertCell(3);
-// let cell5 = row.insertCell(4);
-
-// // print record to page
-// cell1.innerHTML = new Parent(0);
-// cell2.innerHTML = parent2.parentLastName;
-// cell3.innerHTML = new Parent(0);
-// cell4.innerHTML = parent2.childName;
-// cell5.innerHTML = parent2.grade;
-
-/** */
-
-// extra instances of the object
-// const parent1 = new Parent("Joe", "Brown", "joe@email.com", "Suzy", 2);
-// parent1.getInfo();
-
-// const parent2 = new Parent("Sal", "Meyer", "sal@email.com", "Ava", 4);
-// parent2.getInfo();
-
-// const parent3 = new Parent("Sheri", "Jones", "sheri@email.com", "Tommy", 3);
-// parent2.getInfo();
-
-// create columns
-// let row = tableHeader.insertRow(recordCount);
-// let cell1 = row.insertCell(0);
-// let cell2 = row.insertCell(1);
-// let cell3 = row.insertCell(2);
-// let cell4 = row.insertCell(3);
-// let cell5 = row.insertCell(4);
-
-// // print record to page
-// cell1.innerHTML = new Parent(0);
-// cell2.innerHTML = parent1.parentLastName;
-// cell3.innerHTML = new Parent(0);
-// cell4.innerHTML = parent1.childName;
-// cell5.innerHTML = parent1.grade;
-
-//   cell1.innerHTML = new Parent(0);
-//   cell2.innerHTML = parent2.parentLastName;
-//   cell3.innerHTML = new Parent(0);
-//   cell4.innerHTML = parent2.childName;
-//   cell5.innerHTML = parent2.grade;
-
-//     cell1.innerHTML = new Parent(0);
-//     cell2.innerHTML = parent3.parentLastName;
-//     cell3.innerHTML = new Parent(0);
-//     cell4.innerHTML = parent3.childName;
-//     cell5.innerHTML = parent3.grade;
-
-// recordCount++;
-
-
-//inputForm.onsubmit = addRecord();
-
-console.log("i am here");
-
-// inputForm.onsubmit = addRecord();
-
